d128f341395444fbb67af6022d8b7020ce5db6c4
menecing <palaiosman@gmail.com>
2024-02-19 21:05:43 +0400

    methods+

--- Js/job6.js
+++ Js/job6.js
@@ -16,6 +16,7 @@ function returnNeighboringNumbers(num) {
     return [num - 1, num, num + 1];
 }
 returnNeighboringNumbers(5);
+console.log (returnNeighboringNumbers(7));
 
 //3) Создайте функцию, которая будет принимать в себя 2 аргумента, оба числа. 
 //Первое число - это база, второе число - это сколько раз нужно будет повторить это число в прогрессии. 
@@ -25,8 +26,25 @@ returnNeighboringNumbers(5);
 //Если второй аргумент не является числом, равен или меньше нуля - то возвращать 
 //просто первый аргумент. (Проверяем через оператор typeof)
 
-function getMathResult(num,progression) {
-    for ()
+function getMathResult(num, times) {
+    if (typeof(times) !=='number' || times <=0) {
+        return num;
+    }
 
-}    
+    let str = '';
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+            // Тут без черточек в конце
+        } else {
+            str += `${num * i}---`;
+            // Это тоже самое, что и
+            // str = str + num * i + "---"
+        }
+    }
+
+    return str;
+}
+
+console.log (getMathResult(5,5));
 
